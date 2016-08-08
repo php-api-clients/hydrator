9724b6e843e5c986cb351c9d45e30f6ba3908f9b
Cees-Jan Kiewiet <ceesjank@gmail.com>
2016-08-08 22:22:37 +0200

    DateProvider like benchmarks using ParamProviders

--- benchmarks/HydratorBench.php
+++ benchmarks/HydratorBench.php
@@ -2,6 +2,7 @@
 
 use ApiClients\Foundation\Transport\Hydrator;
 use ApiClients\Tests\Foundation\Resources\Sync\Resource;
+use PhpBench\Benchmark\Metadata\Annotations\ParamProviders;
 use PhpBench\Benchmark\Metadata\Annotations\Revs;
 use PhpBench\Benchmark\Metadata\Annotations\Warmup;
 
@@ -11,124 +12,75 @@ use PhpBench\Benchmark\Metadata\Annotations\Warmup;
  */
 class HydratorBench extends AbstractBench
 {
+    public function provideObjects()
+    {
+        return [
+            'complicated' => [
+                'resource' => Resource::class,
+                'json' => [
+                    'id' => 1,
+                    'slog' => 'Wyrihaximus/php-travis-client',
+                    'sub' => [
+                        'id' => 1,
+                        'slug' => 'Wyrihaximus/php-travis-client',
+                    ],
+                    'subs' => [
+                        [
+                            'id' => 1,
+                            'slug' => 'Wyrihaximus/php-travis-client',
+                        ],
+                        [
+                            'id' => 2,
+                            'slug' => 'Wyrihaximus/php-travis-client',
+                        ],
+                        [
+                            'id' => 3,
+                            'slug' => 'Wyrihaximus/php-travis-client',
+                        ],
+                    ],
+                ],
+            ],
+        ];
+    }
+    
     /**
      * @Iterations(10)
      * @Revs(1000)
+     * @ParamProviders({"provideObjects"})
      */
-    public function benchPreHeatedHydrator()
+    public function benchPreHeatedHydrator(array $params)
     {
-        return $this->hydrator->hydrateFQCN(Resource::class, [
-            'id' => 1,
-            'slog' => 'Wyrihaximus/php-travis-client',
-            'sub' => [
-                'id' => 1,
-                'slug' => 'Wyrihaximus/php-travis-client',
-            ],
-            'subs' => [
-                [
-                    'id' => 1,
-                    'slug' => 'Wyrihaximus/php-travis-client',
-                ],
-                [
-                    'id' => 2,
-                    'slug' => 'Wyrihaximus/php-travis-client',
-                ],
-                [
-                    'id' => 3,
-                    'slug' => 'Wyrihaximus/php-travis-client',
-                ],
-            ],
-        ]);
+        return $this->hydrator->hydrateFQCN($params['resource'], $params['json']);
     }
 
     /**
      * @Iterations(10)
      * @Revs(1000)
+     * @ParamProviders({"provideObjects"})
      */
-    public function benchPreHeatedNoCacheHydrator()
+    public function benchPreHeatedNoCacheHydrator(array $params)
     {
-        return $this->hydratorNoCache->hydrateFQCN(Resource::class, [
-            'id' => 1,
-            'slog' => 'Wyrihaximus/php-travis-client',
-            'sub' => [
-                'id' => 1,
-                'slug' => 'Wyrihaximus/php-travis-client',
-            ],
-            'subs' => [
-                [
-                    'id' => 1,
-                    'slug' => 'Wyrihaximus/php-travis-client',
-                ],
-                [
-                    'id' => 2,
-                    'slug' => 'Wyrihaximus/php-travis-client',
-                ],
-                [
-                    'id' => 3,
-                    'slug' => 'Wyrihaximus/php-travis-client',
-                ],
-            ],
-        ]);
+        return $this->hydratorNoCache->hydrateFQCN($params['resource'], $params['json']);
     }
 
     /**
      * @Warmup(10)
      * @Iterations(10)
      * @Revs(1000)
+     * @ParamProviders({"provideObjects"})
      */
-    public function benchFreshCachedHydrator()
+    public function benchFreshCachedHydrator(array $params)
     {
-        return $this->createHydrator()->hydrateFQCN(Resource::class, [
-            'id' => 1,
-            'slog' => 'Wyrihaximus/php-travis-client',
-            'sub' => [
-                'id' => 1,
-                'slug' => 'Wyrihaximus/php-travis-client',
-            ],
-            'subs' => [
-                [
-                    'id' => 1,
-                    'slug' => 'Wyrihaximus/php-travis-client',
-                ],
-                [
-                    'id' => 2,
-                    'slug' => 'Wyrihaximus/php-travis-client',
-                ],
-                [
-                    'id' => 3,
-                    'slug' => 'Wyrihaximus/php-travis-client',
-                ],
-            ],
-        ]);
+        return $this->createHydrator()->hydrateFQCN($params['resource'], $params['json']);
     }
 
     /**
      * @Iterations(10)
      * @Revs(1000)
+     * @ParamProviders({"provideObjects"})
      */
-    public function benchFreshNoCacheHydrator()
+    public function benchFreshNoCacheHydrator(array $params)
     {
-        return $this->createNoCacheHydrator()->hydrateFQCN(Resource::class, [
-            'id' => 1,
-            'slog' => 'Wyrihaximus/php-travis-client',
-            'sub' => [
-                'id' => 1,
-                'slug' => 'Wyrihaximus/php-travis-client',
-            ],
-            'subs' => [
-                [
-                    'id' => 1,
-                    'slug' => 'Wyrihaximus/php-travis-client',
-                ],
-                [
-                    'id' => 2,
-                    'slug' => 'Wyrihaximus/php-travis-client',
-                ],
-                [
-                    'id' => 3,
-                    'slug' => 'Wyrihaximus/php-travis-client',
-                ],
-            ],
-        ]);
+        return $this->createNoCacheHydrator()->hydrateFQCN($params['resource'], $params['json']);
     }
 }
