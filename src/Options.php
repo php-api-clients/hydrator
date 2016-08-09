876c9d8126a3ebeb7201cf681bc1aef226268b2c
Cees-Jan Kiewiet <ceesjank@gmail.com>
2016-08-09 21:56:14 +0200

    Options

---
+++ src/Options.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types=1);
+
+namespace ApiClients\Foundation\Hydrator;
+
+class Options
+{
+    const NAMESPACE =          'namespace';
+    const NAMESPACE_SUFFIX =   'namespace_suffix';
+    const RESOURCE_CACHE_DIR = 'resource_cache_dir';
+    const RESOURCE_NAMESPACE = 'resource_namespace';
+    const EXTRA_PROPERTIES =   'extra_properties';
+    const ANNOTATIONS =        'annotations';
+}
