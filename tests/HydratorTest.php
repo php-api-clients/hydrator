c9f3a5dfebdbcc86e0538e3aff637bd02bf08d42
Cees-Jan Kiewiet <ceesjank@gmail.com>
2016-11-23 23:10:47 +0100

    Test assure we auto fill in missing properties when hydrating

--- tests/HydratorTest.php
+++ tests/HydratorTest.php
@@ -169,4 +169,47 @@ class HydratorTest extends TestCase
 
         $this->assertFalse($classCount === count(get_declared_classes()));
     }
+
+    public function testHydrate()
+    {
+        $json = [
+            'id' => 1,
+            'sub' => [
+                'id' => 1,
+                'slug' => 'Wyrihaximus/php-travis-client',
+            ],
+            'subs' => [
+                [
+                    'id' => 1,
+                    'slug' => 'Wyrihaximus/php-travis-client',
+                ],
+                [
+                    'id' => 2,
+                    'slug' => 'Wyrihaximus/php-travis-client',
+                ],
+                [
+                    'id' => 3,
+                    'slug' => 'Wyrihaximus/php-travis-client',
+                ],
+                [],
+            ],
+        ];
+
+        $loop = LoopFactory::create();
+        $container = ContainerBuilder::buildDevContainer();
+        $container->set(CommandBus::class, $this->createCommandBus($loop));
+        $hydrator = Factory::create($container, [
+            Options::NAMESPACE => 'ApiClients\Tests\Foundation\Hydrator\Resources',
+            Options::NAMESPACE_SUFFIX => 'Async',
+            Options::RESOURCE_CACHE_DIR => $this->getTmpDir(),
+            Options::RESOURCE_NAMESPACE => $this->getRandomNameSpace(),
+        ]);
+        $syncRepository = $hydrator->hydrateFQCN(
+            SyncResource::class,
+            $json,
+            'Async'
+        );
+
+        $this->assertSame(null, $syncRepository->slug());
+    }
 }
